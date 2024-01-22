d9761cdd995792bd777537a0a726d47b5dbb7e96
Samfire2k2 <samuelravard@gmail.com>
2024-01-22 15:16:17 +0100

    2015

--- amazonV3.py
+++ amazonV3.py
@@ -8,7 +8,7 @@ annees_uniques = df[colonne_annee].unique()
 
 year = '2015'
 
-#boucler pour avoir les donnees
+#boucler pour avoir les donnees de 2015
 for year in annees_uniques:
     df_year = df[df[colonne_annee] == 2015]
     df_year.to_csv(f'bestsellers_2015.csv', sep=',' ,index=False, encoding='utf-8')
